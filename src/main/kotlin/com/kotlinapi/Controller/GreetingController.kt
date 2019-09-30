44e66f909d678d78e3d6d54e7a8e2124ba74de6b
Rakes Haridasan <rakesharidas@backwaterbreaks.com>
2019-09-30 13:39:49 +0100

    Minor update to controller to test release pipeline

--- src/main/kotlin/com/kotlinapi/Controller/GreetingController.kt
+++ src/main/kotlin/com/kotlinapi/Controller/GreetingController.kt
@@ -11,7 +11,7 @@ class GreetingController {
     val counter = AtomicLong()
 
     @GetMapping("/greeting")
-    fun greeting(@RequestParam(value = "name", defaultValue = "World, enter a name") name: String) =
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
             Greeting(counter.incrementAndGet(), "Hello, $name")
 
 }
